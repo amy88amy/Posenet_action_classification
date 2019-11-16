8162410e8009ac0da69ceb610b7f87726c0e88db
RichaNagda <richfeel2012@gmail.com>
2019-11-16 01:55:50 -0700

    Euclidean Distance preprocessing

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow import feature_column
 # from keras.layers import Flatten, Dense
 from sklearn.model_selection import train_test_split
 # from tensorflow.keras import layers, models
@@ -12,40 +13,78 @@ import pandas as pd
 label = pd.DataFrame()
 file_path="export_dataframe.csv"
 data=pd.read_csv(file_path)
-label['Label']=data['label']
-data=data.drop('label',1)
 
-x_train, x_test, y_train, y_test = train_test_split(data, label, test_size=0.3)
+train, test = train_test_split(data, test_size=0.99)
+train, val = train_test_split(train, test_size=0.4)
 
-print(x_train.head())
-print(y_train.head())
-print(x_test.head())
-print(y_test.head())
+print(len(train), 'train examples')
+print(len(val), 'validation examples')
+print(len(test), 'test examples')
 
-print(x_train.shape)
-print(y_train.shape)
-print(x_test.shape)
-print(y_test.shape)
+
+def df_to_dataset(dataframe, shuffle=True):
+  dataframe = dataframe.copy()
+  labels = dataframe.pop('Label')
+  ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+  if shuffle:
+    ds = ds.shuffle(buffer_size=len(dataframe))
+  return ds
+
+
+train_ds = df_to_dataset(train) 
+val_ds = df_to_dataset(val, shuffle=False) 
+test_ds = df_to_dataset(test, shuffle=False) 
+
+# print(train_ds)
+
+for feature_batch, label_batch in train_ds.take(1):
+#   print('Every feature:', list(feature_batch.keys()))
+  feature_columns = []
+  for feature in feature_batch.keys():
+    #   feature_column = feature_batch[feature]
+      feature_columns.append(feature_column.numeric_column(feature))
+  feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+
+# label['Label']=data['label']
+# data=data.drop('label',1)
+
+# x_train, x_test, y_train, y_test = train_test_split(data, label, test_size=0.3)
+
+# print(x_train.head())
+# print(y_train.head())
+# print(x_test.head())
+# print(y_test.head())
+
+# print(x_train.shape)
+# print(y_train.shape)
+# print(x_test.shape)
+# print(y_test.shape)
 
 class_names = ['book', 'car', 'gift', 'movie', 'sell', 'total']
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Conv2D(16, (17, 2), (2, 1), activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D((4, 2)))
-model.add(tf.keras.layers.Conv2D(32, (6, 2), (2, 1), activation='relu'))
-model.add(tf.keras.layers.Conv2D(64, (2, 5), (2, 1), activation='relu'))
-# model.add(Flatten())
-model.add(Dense(units=34, activation='softmax'))
+model = tf.keras.models.Sequential([feature_layer])
 
+
+# model.add(tf.keras.layers.Conv2D(16, (12, 2), activation='relu'))
+# model.add(tf.keras.layers.MaxPooling2D((8, 8)))
+# model.add(tf.keras.layers.Conv2D(8, (6, 2), activation='relu'))
+# model.add(tf.keras.layers.MaxPooling2D((8, 8)))
+# model.add(tf.keras.layers.Conv2D(64, (2, 5), activation='relu'))
+
+model.add(tf.keras.layers.Dense(units=15, activation='relu'))
+model.add(tf.keras.layers.Dense(units=10, activation='relu'))
+model.add(tf.keras.layers.Dense(units=6, activation='softmax'))
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
 
-test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
+history = model.fit(train_ds, batch_size=32, validation_data=val_ds, epochs=2)
 
-print(test_acc)
+# test_loss, test_acc = model.evaluate(test_ds)
+
+# print(test_acc)
+print(history.accuracy, "   Accuracy of train")
 print(model.summary())
 
--- preprocessing.py
+++ preprocessing.py
@@ -1,32 +1,50 @@
 
 import os
 import pandas as pd
-
-label = pd.DataFrame(columns=['Label'])
+import numpy as np
+import math
 data = pd.DataFrame()
-
+label = pd.DataFrame(columns=['Label'])
 root_dir = os.getcwd() + "/CSV"
 lab = []
+df_xx=pd.DataFrame()
+df_yy=pd.DataFrame()
+df_x=pd.DataFrame()
+df_y=pd.DataFrame()
+dist=pd.DataFrame()
 i = 0
 for subdir, dirs, files in os.walk(root_dir):
     for file in files:
         path = os.path.join(subdir, file)
         l = path.split('/')
-
+        
         if l[-2] not in lab:
             lab.append(l[-2])
             i += 1
-        
+        # print(l[-2])
         df = pd.read_csv(path)
-        df=df.drop(df.columns.difference(['leftShoulder_x','leftShoulder_y','rightShoulder_x',
-        'rightShoulder_y','leftElbow_x','leftElbow_y','rightElbow_x','rightElbow_y','leftWrist_x',
-        'leftWrist_y','rightWrist_x','rightWrist_y',]), 1)
-      
+        
+        df_xx=df.drop(df.columns.difference(['leftShoulder_x','rightShoulder_x',
+        'leftElbow_x','rightElbow_x','leftWrist_x','rightWrist_x','nose_x',]), 1)
+        df_yy=df.drop(df.columns.difference(['leftShoulder_y','rightShoulder_y','leftElbow_y',
+        'rightElbow_y','leftWrist_y','rightWrist_y','nose_y']), 1)
+       
         for k in range(0, df.shape[0]):
             label.loc[len(label.index), 'Label'] = int(i)
-        data = pd.concat([data, df])
-        data.append(label)
+        df_x = pd.concat([df_x, df_xx])
+        df_y = pd.concat([df_y, df_yy])
+        
+   
+df_x=df_x.reset_index(drop=True)
+df_y=df_y.reset_index(drop=True)
 
-data['label']=label['Label']
+for i in range(1,len(df_x.columns)):
+    dist[df_x.columns[i]]=np.sqrt((df_x[df_x.columns[0]]-df_x[df_x.columns[i]])**2
+                              +(df_y[df_y.columns[0]]-df_y[df_y.columns[i]])**2)
 
-export_csv = data.to_csv (r'export_dataframe.csv', index = None, header=True)
+dist = dist.reset_index(drop=True)
+label = label.reset_index(drop=True)
+# print(dist.shape)
+dist['Label']=label
+# print(dist.shape)
+dist.to_csv (r'export_dataframe.csv', index = None, header=True)
